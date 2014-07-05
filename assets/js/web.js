f610bdc0f93ddd6f41a66dc119bee3a837f10f9b
Subhajit Sahu <wolfram77@gmail.com>
2014-07-06 00:01:18 +0530

    to update status system

--- assets/js/web.js
+++ assets/js/web.js
@@ -22,11 +22,46 @@ web.controller('ApiController', ['$http', function($http) {
 // Directives
 // ----------
 
+// status-system directive
+web.directive('statusSystem', function() {
+	return {
+		restrict: 'E',
+		templateUrl: '/html/status-system.html'
+	};
+});
+
+
 // status-header directive
 web.directive('statusHeader', function() {
 	return {
 		restrict: 'E',
-		templateUrl: '/html/status-header.html'
+		templateUrl: '/html/status-header.html',
+		controller: function() {
+			var obj = this;
+			obj.menu = { 'value': 0 };
+			obj.button = { 'value': false };
+
+			// select menu
+			this.menu.select = function(val) {
+				obj.menu.value = val;
+			};
+
+			// is menu selected?
+			this.menu.is = function(val) {
+				return obj.menu.value === val;
+			};
+
+			// toggle button
+			this.button.toggle = function() {
+				obj.button.value = !obj.button.value;
+			};
+
+			// is button selected?
+			this.button.is = function() {
+				return obj.button.value;
+			};
+		},
+		controllerAs: 'stHdr'
 	};
 });
 
@@ -37,19 +72,20 @@ web.directive('webHeader', function () {
 		restrict: 'E',
 		templateUrl: '/html/web-header.html',
 		controller: function() {
-			this.value = 0;
+			var obj = this;
+			obj.value = 0;
 
 			// select menu
-			this.set = function(val) {
-				this.value = val;
+			this.select = function(val) {
+				obj.value = val;
 			}
 
 			// is selected?
 			this.is = function(val) {
-				return this.value === val;
+				return obj.value === val;
 			}
 		},
-		controllerAs: 'menu'
+		controllerAs: 'webHdr'
 	};
 });
 
